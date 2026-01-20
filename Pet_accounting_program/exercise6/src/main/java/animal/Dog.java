0b4bc42e92b1adf93507a4d8940d9d76eaf1cc33
faraldma <faraldma@student.21-school.ru>
2026-01-20 17:26:32 +0700

    add sixth exersice

---
+++ Pet_accounting_program/exercise6/src/main/java/animal/Dog.java
@@ -0,0 +1,14 @@
+package animal;
+
+public class Dog extends Animal {	
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Dog name = " + getName() + ", age = " + getAge();
+    }	    
+}
+
+
---
+++ Pet_accounting_program/exercise6/src/main/java/task6/Main.java
@@ -0,0 +1,63 @@
+package task6;
+
+import animal.Animal;
+import animal.Dog;
+import animal.Cat;
+import iterator.AnimalIterator;
+import utils.PScanner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.InputMismatchException;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+import java.util.Objects;
+
+public class Main {
+    private static PScanner scanner;
+
+    public static void main(String[] args) {
+        List<Animal> pets;
+        int numberOfPets;
+        scanner = new PScanner(System.in);
+	
+        try {
+            numberOfPets = scanner.nextPositiveInt();
+            pets = IntStream.range(0, numberOfPets).mapToObj(pet -> scanPet()).filter(Objects::nonNull).collect(Collectors.toList());
+
+            AnimalIterator animalIterator = new AnimalIterator(pets);
+            while (animalIterator.hasNext()) {
+                System.out.println(animalIterator.next());
+            }
+        } catch (InputMismatchException e) {
+	        System.err.println(e.getMessage() + ". Please try again");
+        }
+
+    }
+
+    public static Animal scanPet() {
+	    Animal theAnimal = null;
+        String name;
+	    int age;
+        try {
+            if (scanner.hasNext()) {
+                switch (scanner.next()) {
+                    case "dog": 
+                        name = scanner.next();
+                        age = scanner.nextPositiveInt();
+                        theAnimal = new Dog(name, age);
+                        break;
+                    case "cat":
+                        name = scanner.next();
+                        age = scanner.nextPositiveInt();
+                        theAnimal = new Cat(name, age);
+                        break;
+                    default: System.out.println("Incorrect input. Unsupported pet type");
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.err.println(e.getMessage() + ". Please try again");
+        }
+	    return theAnimal;
+    }
+}
